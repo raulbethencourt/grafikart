e3a9dcb830f973c7cdb905479b229093cd64fd32
Raul <raul.bethencourt.pro@gmail.com>
2020-09-04 10:55:29 +0200

    :sparkles: I start OOP tutorial

---
+++ php/Blog/pages/home.html.php
@@ -0,0 +1,5 @@
+<h1>Je suis la homepage</h>
+
+<a name="single" id="single" class="btn btn-secondary" href="index.php?p=single" role="button">Single</a>
+
+
---
+++ php/Blog/pages/templates/base.html.php
@@ -0,0 +1,32 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <title>Starter Template · Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="index.php?p=home">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
+            aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+</nav>
+
+<main role="main" class="container" style="padding-top: 100px">
+    <div class="starter-template">
+        <?= $content; ?>
+    </div>
+
+</main>
+</body>
+</html>
