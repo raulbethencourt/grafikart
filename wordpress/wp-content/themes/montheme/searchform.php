be8469732c0efe7072c85c0790542e8dceed068d
Raul <raul.bethencourt.pro@gmail.com>
2020-10-14 22:42:21 +0200

    :fire: remove Wordpress

--- wordpress/wp-content/themes/montheme/searchform.php
+++
@@ -1,4 +0,0 @@
-<form class="form-inline my-2 my-lg-0" action="<?= esc_url(home_url('/')); ?>">
-    <input class="form-control mr-sm-2" name="s" type="search" placeholder="Recherche" aria-label="Search" value="<?= get_search_query(); ?>">
-    <button class="btn btn-outline bg-light my-2 my-sm-0" type="submit">Rechercher</button>
-</form>
