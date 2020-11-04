923443310c3729ac7e6ab1636779147f0c68baf3
Raul <raul.bethencourt.pro@gmail.com>
2020-11-04 23:29:16 +0100

    :construction: Tool class for Array

--- php/Blog/pages/posts/home.html.php
+++ php/Blog/pages/posts/home.html.php
@@ -1,7 +1,24 @@
+<?php
+
+use Core\Collection\Collection;
+
+$posts = App::getInstance()->getTable('post')->all();
+
+$test = new Collection([
+    ['name' => 'Jean', 'note' => 20, 'nickname' => 'test'],
+    ['name' => 'Doty', 'note' => 30, 'nickname' => 'test2'],
+    ['name' => 'Paul', 'note' => 10, 'nickname' => 'test3'],
+]);
+
+var_dump($test->__extract('note')->__join(', '));
+exit();
+
+?>
+
 <div class="row">
     <div class="col-sm-8">
         <?php
-        foreach (App::getInstance()->getTable('post')->last() as $post): ?>
+        foreach (App::getInstance()->getTable('post')->last() as $post) : ?>
 
             <h2><a href="<?= $post->url ?>"><?= $post->titre ?></a></h2>
 
@@ -14,11 +31,10 @@
 
     <div class="col-sm-4">
         <ul>
-            <?php foreach (App::getInstance()->getTable('category')->all() as $category): ?>
+            <?php foreach (App::getInstance()->getTable('category')->all() as $category) : ?>
                 <li><a href="<?= $category->url ?>"><?= $category->title ?></a></li>
             <?php endforeach ?>
         </ul>
 
     </div>
 </div>
-
